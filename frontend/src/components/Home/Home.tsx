680dd5d3fd86df65924a9d0c0c28f6cfa38bdcbd
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-10-27 01:06:01 +0530

    fix(Home): update user context usage and improve button navigation logic

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,41 +1,57 @@
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { useUserContext } from "@/context/context";
+import { ArrowRight } from "lucide-react";
+import { Fragment, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
 	useEffect(() => {
 		window.document.title = "Home - Clouly";
 	}, []);
+	const { user } = useUserContext();
 	return (
-		<div className="bg-background text-foreground flex flex-col min-h-[80svh] max-w-6xl mx-auto justify-center items-center px-4 sm:px-6 text-center">
-			<h1 className="text-3xl sm:text-4xl md:text-6xl leading-snug sm:leading-tight">
-				<span className="text-foreground">Unlimited Free&nbsp;</span>
-				<span className="text-primary">Subdomains</span>
-				<br className="block" />
-				<span className="text-foreground">with Clouly</span>
-			</h1>
+		<Fragment>
+			<div className="bg-background text-foreground flex flex-col min-h-[80svh] max-w-6xl mx-auto justify-center items-center px-4 sm:px-6 text-center">
+				<h1 className="text-3xl sm:text-4xl md:text-6xl leading-snug sm:leading-tight">
+					<span className="text-foreground">
+						Unlimited Free&nbsp;
+					</span>
+					<span className="text-primary">Subdomains</span>
+					<br className="block" />
+					<span className="text-foreground">
+						with{" "}
+						<span className="font-semibold text-cyan-500">
+							Clouly
+						</span>
+					</span>
+				</h1>
 
-			<p className="mt-4 sm:mt-6 text-sm sm:text-base md:text-xl text-muted-foreground max-w-lg sm:max-w-2xl">
-				Clouly lets you create and manage subdomains under{" "}
-				<strong>clouly.in</strong> - a fast, secure, and
-				developer-friendly way to keep all your subdomains in one place.
-			</p>
+				<p className="mt-4 sm:mt-4 text-sm sm:text-base md:text-lg text-muted-foreground max-w-lg sm:max-w-2xl">
+					Clouly lets you create and manage subdomains under{" "}
+					<strong className="font-semibold">clouly.in</strong> - a
+					fast, secure, and developer-friendly way to keep all your
+					subdomains in one place.
+				</p>
 
-			<div className="mt-6 sm:mt-8 flex sm:flex-row gap-3 sm:gap-4 justify-center">
-				<Button
-					className="text-sm sm:text-lg px-5 sm:px-6 py-2 sm:py-3 bg-primary text-background hover:bg-primary/90 duration-200 transition-all hover:scale-105"
-					asChild
-				>
-					<Link to="/dashboard">Start for Free</Link>
-				</Button>
-				<Button
-					variant="outline"
-					className="text-sm sm:text-lg px-5 sm:px-6 py-2 sm:py-3 hover:bg-accent/20"
-					asChild
-				>
-					<Link to="/learn-more">How It Works</Link>
-				</Button>
+				<div className="mt-6 sm:mt-8 flex sm:flex-row gap-3 sm:gap-4 justify-center">
+					<Button
+						className="text-sm sm:text-lg px-5 sm:px-6 py-2 sm:py-3 bg-primary text-background hover:bg-primary/90 duration-200 transition-all hover:scale-105"
+						asChild
+					>
+						<Link to={user ? "/dashboard" : "/login"}>
+							Start for Free
+							<ArrowRight className="" />
+						</Link>
+					</Button>
+					<Button
+						variant="outline"
+						className="text-sm sm:text-lg px-5 sm:px-6 py-2 sm:py-3 hover:bg-accent/20"
+						asChild
+					>
+						<Link to="/learn-more">How It Works</Link>
+					</Button>
+				</div>
 			</div>
-		</div>
+		</Fragment>
 	);
 };
